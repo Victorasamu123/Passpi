2e593f37c259a3d7dc62727b37d18293a86d6756
Avanin <asamuvictor03@gmail.com>
2023-12-19 23:40:46 +0100

    configure sessions and connected to mongoose

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const cors_1 = __importDefault(require("cors"));
+const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 require("dotenv").config();
 const PORT = process.env.PORT || 4500;
+const MONGODB = process.env.MONGODB;
+// const dbstring: any = MONGODB;
+// const dbOptions: any = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology:true
+// }
+// const conection = mongoose.connect(MONGODB as string);
+const store = new connect_mongo_1.default({
+    mongoUrl: MONGODB,
+    collectionName: "sessions"
+});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// const sessionStore= new MongoStore({
+//     mongooseConnection : conection,
+//     collection:"session"
+// });
+app.use((0, express_session_1.default)({
+    secret: "same secret",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
 app.get("/", (req, res) => {
     res.send("");
 });
--- index.ts
+++ index.ts
@@ -1,9 +1,38 @@
 import express, {Express, Request, Response} from "express"
+import mongoose, { connection } from "mongoose";
+import session from "express-session";
+import cors from "cors";
+import MongoStore from "connect-mongo";
+
+
+
 const app = express();
+
+app.use(cors())
+
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4500
+const MONGODB = process.env.MONGODB
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const store = new MongoStore({
+    mongoUrl: MONGODB,
+    collectionName:"sessions"
+});
+
+app.use(session({
+    secret:"same secret",
+    resave:false,
+    saveUninitialized:true,
+    store: store,
+    cookie:{
+        maxAge: 1000 * 60 * 60 *24
+    }
+}));
 
 app.get("/", (req :Request,res:Response)=>{
 res.send( "");
@@ -14,5 +43,5 @@ app.get("/justy", (req:Request, res:Response)=>{
 });
 
 app.listen(PORT , ()=>{
-    console.log(`port listening at port ${PORT}`)
+    console.log(`port listening at port ${PORT}`);
 });
