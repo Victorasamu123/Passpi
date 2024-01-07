38b3ba40092e388ebf91c01143bc0b79898bcb2b
Avanin <asamuvictor03@gmail.com>
2024-01-07 21:42:42 +0100

    auth route

---
+++ routes/auth.route.ts
@@ -0,0 +1,7 @@
+import express,{Request,Response,NextFunction} from "express";
+import passport from "passport";
+import { validPassword } from "../lib/passwordUtils";
+import { genPassword } from "../lib/passwordUtils";
+import { User } from "../config/database";
+
+const auth = express.Router();
