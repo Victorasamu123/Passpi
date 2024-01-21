5fd7dd46ea24974c9bcfb6456ccb912a8683ca88
Avanin <asamuvictor03@gmail.com>
2024-01-21 21:17:35 +0100

    updated route

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -6,8 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = __importDefault(require("express"));
 exports.router = express_1.default.Router();
+// post routess //
 exports.router.post("/signin", (req, res, next) => { });
 exports.router.post("/signup", (req, res, next) => { });
-exports.router.post("/", (req, res, next) => {
-    // res.send
+//  get route //
+exports.router.get("/", (req, res, next) => {
+    res.send(`<h1>Home</h1><p>Please <a href="/signup">register</a></p>`);
+});
+exports.router.get("/login", (req, res, next) => {
+    const form = `<<h1>Login Page</h1><form method="POST" action="/signin">\ Enter Username:<br><input type="text" name="username">\ <br>Enter Password:<br><input type="password" name="password">\ <br><br><input type="submit" value="Submit"></form>`;
+    res.send(form);
+});
+exports.router.get("/register", (req, res, next) => {
+    const form = `<<h1>Register Page</h1><form method="POST" action="/signup">\ Enter Username:<br><input type="text" name="username">\ <br>Enter Password:<br><input type="password" name="password">\ <br><br><input type="submit" value="Submit"></form>`;
+    res.send(form);
 });
--- routes/index.ts
+++ routes/index.ts
@@ -6,16 +6,37 @@ import { User } from "../config/database";
 
 export const router = express.Router();
 
+// post routess //
+
 router.post("/signin",(req:Request,res:Response,next:NextFunction)=>{});
 
 router.post("/signup",(req:Request,res:Response,next:NextFunction)=>{});
 
-router.post("/",(req:Request,res:Response,next:NextFunction)=>{
-    // res.send
+//  get route //
+
+router.get("/" , (req:Request, res:Response, next:NextFunction)=>{
+    res.send(`<h1>Home</h1><p>Please <a href="/signup">register</a></p>`)
 });
-router.post("/hello",(req:Request,res:Response,next:NextFunction)=>{
-    // res.send
+
+router.get("/login" , (req:Request, res:Response, next:NextFunction)=>{
+    const form = `<<h1>Login Page</h1><form method="POST" action="/signin">\ Enter Username:<br><input type="text" name="username">\ <br>Enter Password:<br><input type="password" name="password">\ <br><br><input type="submit" value="Submit"></form>`
+
+    res.send(form);
+});
+
+router.get("/register" , (req:Request, res:Response, next:NextFunction)=>{
+    const form = `<<h1>Register Page</h1><form method="POST" action="/signup">\ Enter Username:<br><input type="text" name="username">\ <br>Enter Password:<br><input type="password" name="password">\ <br><br><input type="submit" value="Submit"></form>`
+
+    res.send(form);
+});
+router.get("/register" , (req:Request, res:Response, next:NextFunction)=>{
+    const form = `<<h1>Register Page</h1><form method="POST" action="/signup">\ Enter Username:<br><input type="text" name="username">\ <br>Enter Password:<br><input type="password" name="password">\ <br><br><input type="submit" value="Submit"></form>`
+      
+    res.send(form);
 });
 
 
 
+
+
+ 
