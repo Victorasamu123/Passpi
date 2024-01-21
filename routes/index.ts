6a52760d71589e6237fcb028e759e524d78b6a73
Avanin <asamuvictor03@gmail.com>
2024-01-21 22:14:20 +0100

    configure passport-local stra

--- routes/index.ts
+++ routes/index.ts
@@ -35,8 +35,55 @@ router.get("/register" , (req:Request, res:Response, next:NextFunction)=>{
     res.send(form);
 });
 
+/**
+ * Lookup how to authenticate users on routes with Local Strategy
+ * Google Search: "How to use Express Passport Local Strategy"
+ * 
+ * Also, look up what behaviour express session has without a maxage set
+ */
+
+
+router.get('/protected-route', (req, res, next) => {
+    
+    // This is how you check if a user is authenticated and protect a route.  You could turn this into a custom middleware to make it less redundant
+    if (req.isAuthenticated()) {
+        res.send('<h1>You are authenticated</h1><p><a href="/logout">Logout and reload</a></p>');
+    } else {
+        res.send('<h1>You are not authenticated</h1><p><a href="/login">Login</a></p>');
+    }
+});
+
+// Visiting this route logs the user out
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+          console.error('Error logging out:', err);
+          res.status(500).send('Error logging out');
+        } else {
+          req.session.destroy((err) => {
+            if (err) {
+              console.error('Error destroying session:', err);
+              res.status(500).send('Error destroying session');
+            } else {
+              res.redirect('/'); // Redirect to the homepage or login page
+            }
+          });
+        }
+      });
+    res.redirect('/protected-route');
+});
+
+router.get('/login-success', (req, res, next) => {
+    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
+});
+
+router.get('/login-failure', (req, res, next) => {
+    res.send('You entered the wrong password.');
+});
 
 
 
 
- 
+
+
+  
