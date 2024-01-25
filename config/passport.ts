90479537310e07d6d69edb873f7fcbf275eed557
Avanin <asamuvictor03@gmail.com>
2024-01-25 22:27:07 +0100

    updated local-sta

--- config/passport.ts
+++ config/passport.ts
@@ -4,15 +4,37 @@ import {User} from "./database"
 import { validPassword } from "../lib/passwordUtils";
 const LocalStrategy = localStrategy.Strategy;
 
+const customFields = {
+    usernameField : "uname",
+    passwordField:"pw"
+};
+
 const verifyCallBack = (username:any, password:any, done:any) => {
     
+    User.findOne({username:username}).then((user:any)=>{
+
+        if(!user){return cb(null, false)}
+
+        //function defined 
+        const isValid = validPassword(password,user.hash, user.salt);
+
+        if(isValid){
+            return cb(null, user);
+        } else {
+            return cb(null , false);
+        }
+
+    }).catch((err)=>{
+        cb(err);
+    })
+
 } 
 
 // const Strategy = new LocalStrategy()
 
 passport.use(new LocalStrategy(
     function(username, password, cb){
-        User.findOne({username:username}).then((user)=>{
+     User.findOne({username:username}).then((user)=>{
 
             if(!user){return cb(null, false)}
 
@@ -21,3 +43,7 @@ passport.use(new LocalStrategy(
         })
     }
 ))
+
+function cb(arg0: null, arg1: boolean): any {
+    throw new Error("Function not implemented.");
+}
--- dist/config/passport.js
+++ dist/config/passport.js
@@ -8,7 +8,26 @@ const passport_local_1 = __importDefault(require("passport-local"));
 const database_1 = require("./database");
 const passwordUtils_1 = require("../lib/passwordUtils");
 const LocalStrategy = passport_local_1.default.Strategy;
+const customFields = {
+    usernameField: "uname",
+    passwordField: "pw"
+};
 const verifyCallBack = (username, password, done) => {
+    database_1.User.findOne({ username: username }).then((user) => {
+        if (!user) {
+            return cb(null, false);
+        }
+        //function defined 
+        const isValid = (0, passwordUtils_1.validPassword)(password, user.hash, user.salt);
+        if (isValid) {
+            return cb(null, user);
+        }
+        else {
+            return cb(null, false);
+        }
+    }).catch((err) => {
+        cb(err);
+    });
 };
 // const Strategy = new LocalStrategy()
 passport_1.default.use(new LocalStrategy(function (username, password, cb) {
@@ -20,3 +39,6 @@ passport_1.default.use(new LocalStrategy(function (username, password, cb) {
         const isValid = (0, passwordUtils_1.validPassword)(password, user.hash, user.salt);
     });
 }));
+function cb(arg0, arg1) {
+    throw new Error("Function not implemented.");
+}
