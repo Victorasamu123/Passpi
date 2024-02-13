c5dd6f12259a19fecde84e40c8884b29a7a649c0
Avanin <asamuvictor03@gmail.com>
2024-02-13 22:01:37 +0100

    created new __

--- Cryptography/createKeyPair.ts
+++ Cryptography/createKeyPair.ts
@@ -1,22 +1,44 @@
 import crypto from "crypto";
 import fs from "fs"
+import express , {Response} from "express";
 
-const genKeyPair=():void=>{
-    const keyPair= crypto.generateKeyPairSync('x448',{
-        modulusLength:4906,
-        PublicKeyEncoding:{
-            type:"pkcs1",
-            format:"pem"
-        },
-        privateKeyEncoding:{
-            type:"pkcs1",
-            format:"pem"
-        }
-    });
+// const generateKeyPair():{publicKey:string ,privateKey: string}{
+//   const {privateKey, publicKey} = 
+// }
 
-    const publicKeyPem = keyPair.publicKey.export({ type: 'pkcs1', format: 'pem' });
-    fs.writeFileSync(__dirname + '/id_rsa_pub.pem',publicKeyPem);
+const {privateKey, publicKey} = crypto.generateKeyPairSync('rsa',{
+   modulusLength:4096,
+   publicKeyEncoding:{
+    type:'spki',
+    format:'pem'
+   },
+   privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem'
+  }
+});
 
-    const privateKeyPem = keyPair.privateKey.export({ type: 'pkcs1', format: 'pem' });
-    fs.writeFileSync(__dirname + '/id_rsa_priv.pem',privateKeyPem);
-}
+fs.writeFileSync('privateKey.pem',privateKey);
+fs.writeFileSync('publicKey.pem',publicKey);
+
+
+
+// export const genKeyPair=():void=>{
+//     const keyPair= crypto.generateKeyPairSync('x448',{
+//         modulusLength:4906,
+//         PublicKeyEncoding:{
+//             type:"pkcs1",
+//             format:"pem"
+//         },
+//         privateKeyEncoding:{
+//             type:"pkcs1",
+//             format:"pem"
+//         }
+//     });
+//      console.log(keyPair.privateKey);
+//     const publicKeyPem = keyPair.publicKey.export({ type: 'pkcs1', format: 'pem' });
+//     fs.writeFileSync(__dirname + '/id_rsa_pub.pem',publicKeyPem);
+
+//     const privateKeyPem = keyPair.privateKey.export({ type: 'pkcs1', format: 'pem' });
+//     fs.writeFileSync(__dirname + '/id_rsa_priv.pem',privateKeyPem);
+// }
--- dist/Cryptography/createKeyPair.js
+++ dist/Cryptography/createKeyPair.js
@@ -5,20 +5,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const crypto_1 = __importDefault(require("crypto"));
 const fs_1 = __importDefault(require("fs"));
-const genKeyPair = () => {
-    const keyPair = crypto_1.default.generateKeyPairSync('x448', {
-        modulusLength: 4906,
-        PublicKeyEncoding: {
-            type: "pkcs1",
-            format: "pem"
-        },
-        privateKeyEncoding: {
-            type: "pkcs1",
-            format: "pem"
-        }
-    });
-    const publicKeyPem = keyPair.publicKey.export({ type: 'pkcs1', format: 'pem' });
-    fs_1.default.writeFileSync(__dirname + '/id_rsa_pub.pem', publicKeyPem);
-    const privateKeyPem = keyPair.privateKey.export({ type: 'pkcs1', format: 'pem' });
-    fs_1.default.writeFileSync(__dirname + '/id_rsa_priv.pem', privateKeyPem);
-};
+// const generateKeyPair():{publicKey:string ,privateKey: string}{
+//   const {privateKey, publicKey} = 
+// }
+const { privateKey, publicKey } = crypto_1.default.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem'
+    }
+});
+fs_1.default.writeFileSync('privateKey.pem', privateKey);
+fs_1.default.writeFileSync('publicKey.pem', publicKey);
+// export const genKeyPair=():void=>{
+//     const keyPair= crypto.generateKeyPairSync('x448',{
+//         modulusLength:4906,
+//         PublicKeyEncoding:{
+//             type:"pkcs1",
+//             format:"pem"
+//         },
+//         privateKeyEncoding:{
+//             type:"pkcs1",
+//             format:"pem"
+//         }
+//     });
+//      console.log(keyPair.privateKey);
+//     const publicKeyPem = keyPair.publicKey.export({ type: 'pkcs1', format: 'pem' });
+//     fs.writeFileSync(__dirname + '/id_rsa_pub.pem',publicKeyPem);
+//     const privateKeyPem = keyPair.privateKey.export({ type: 'pkcs1', format: 'pem' });
+//     fs.writeFileSync(__dirname + '/id_rsa_priv.pem',privateKeyPem);
+// }
